7c6313a8a7c489cc0a508082eeee7a7d1edf0ab5
orestes1986 <orestes.k.poulakis@gmail.com>
2018-07-13 05:03:54 +0300

    removed the app/server/port lines from server file

--- server/main.js
+++ server/main.js
@@ -1,8 +1,4 @@
 // code that is only sent to the server. 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
 
 Meteor.startup(function () {
 //	 create a starter doc if necessary
